245866978c3e9628156a8a46a6909db9cd2e9581
Kranti Kumar Shah <Krantikumar803@gmail.com>
2025-12-15 20:00:07 +0545

    login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from "@/components/auth/LoginForm"
+import { Suspense } from "react"
 
 export default function LoginPage() {
   return (
     <main className="min-h-screen grid place-items-center p-4">
-      <LoginForm />
+      <Suspense fallback={<p>Loading...</p>}>
+        <LoginForm />
+      </Suspense>
     </main>
   )
 }
